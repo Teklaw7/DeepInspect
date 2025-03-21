c1e7bcd0622679f40ae6b1b8582af11e4e1248e1
Teklaw7 <timothee.teyssier@cpe.fr>
2025-03-21 15:52:10 +0100

    add a simple home page with a qline edit

---
+++ app/homepage.hpp
@@ -0,0 +1,24 @@
+#ifndef HOMEPAGE_HPP
+#define HOMEPAGE_HPP
+
+#include <QWidget>
+
+namespace Ui
+{
+    class HomePage;
+}
+
+
+class HomePage : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit HomePage(QWidget *parent = nullptr);
+    ~HomePage();
+
+private:
+    Ui::HomePage *m_ui;
+};
+
+#endif // HOMEPAGE_HPP
