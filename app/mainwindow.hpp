1dc62f664c5cfdddbb8d483ce723cebd77e8161e
Teklaw7 <timothee.teyssier@cpe.fr>
2025-03-21 15:03:16 +0100

    mainwindow.h -> mainwindow.hpp

--- app/mainwindow.hpp
+++ app/mainwindow.hpp
@@ -1,5 +1,5 @@
-#ifndef MAINWINDOW_H
-#define MAINWINDOW_H
+#ifndef MAINWINDOW_HPP
+#define MAINWINDOW_HPP
 
 #include "header.hpp"
 
@@ -23,4 +23,4 @@ private:
     Ui::MainWindow * m_ui;
     Header * m_header;
 };
-#endif // MAINWINDOW_H
+#endif // MAINWINDOW_HPP
